42c784dfd7dbfd7de232e248e27fc11be12f6cce
casjora <casjora@gmail.com>
2026-06-06 09:11:17 -0600

    A ver si da vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        checklist: resolve(__dirname, 'windbnb_checklist_tailwind.html'),
+      },
+    },
+  },
 })
