53206f6a445372dc8e2d9148025bd322faa41687
casjora <casjora@gmail.com>
2026-06-15 10:34:04 -0600

    Corrigiendo logica

--- src/scripts/prueba.js
+++ src/scripts/prueba.js
@@ -41,10 +41,14 @@ function simuladorDeCombate(){
     let ptosVida=parseInt(prompt("Cuantos puntos de vida tendra el jugador?"))
     let enemyAttack = parseInt(prompt("Cuantos puntos de ataque tendra el enemigo?"))
 
+    let contador = 1
     console.log(`la vida del usuario es ${ptosVida}`)
     while (ptosVida >=10) {
+
         ptosVida=ptosVida-enemyAttack
+        console.log(`Resultado del ataque ${contador}` )
         alert("Sobreviviste")
+        contador++
     }
     alert("¡Peligro crítico!")
 }
